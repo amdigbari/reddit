26e9b7a550b8668a30618efc6d90382fa6c4ba58
amir <amirmahdidigbari@yahoo.com>
2019-12-16 12:11:18 +0330

    change some colors

--- src/utils/staticUtils.js
+++ src/utils/staticUtils.js
@@ -5,8 +5,10 @@ export const SEARCH_DELAY = 500;
 export const ANIMATION_DURATION = 400;
 
 // Colors
-export const GRAY = '#555';
-
+export const GRAY = '#4a4a4a';
+export const PRIMARY_COLOR = '#ff6265';
+export const BACKGROUND_COLOR = '#f1efea';
+export const LIGHT_COLOR = '#fff';
 
 //Sizes
 export const ICON_MOBILE_SIZE = 30;
