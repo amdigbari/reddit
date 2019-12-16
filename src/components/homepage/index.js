6e6c2bed207edd97e3adde811067dbb0692b7f63
amir <amirmahdidigbari@yahoo.com>
2019-12-16 13:49:12 +0330

    add simple tab navigator

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 import CustomScreen from '../common/customScreen/CustomScreen';
+import CustomNavbar from '../common/navbar/CustomNavbar';
+import { navigationTabs } from '../../utils/hardcodedData';
 
 const Homepage = React.memo(props => {
-    return <CustomScreen>Homepage</CustomScreen>;
+    const renderComponent = React.useCallback(activeNavigatorId => {
+        return <div>{navigationTabs.find(navigator => navigator.id === activeNavigatorId).title}</div>;
+    }, []);
+
+    return (
+        <CustomScreen>
+            <CustomNavbar navigators={navigationTabs} renderComponent={renderComponent} />
+        </CustomScreen>
+    );
 });
 
 export default Homepage;
---
+++ src/utils/hardcodedData.js
@@ -0,0 +1,6 @@
+export const navigationTabs = [
+    { id: 1, title: 'Following' },
+    { id: 2, title: 'Hots' },
+    { id: 3, title: 'News' },
+    { id: 4, title: 'Activities' },
+];
